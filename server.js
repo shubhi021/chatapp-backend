286b160c60ddc70d8348b7b7e4f9a510c846f930
shubhi021 <srivastavashubhi559@gmail.com>
2026-05-29 11:26:31 +0530

    Initial backend setup

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room ${roomId}`);
+  });
+
+  socket.on("send_message", (data) => {
+    io.to(data.roomId).emit("receive_message", data);
+  });
+  socket.on("typing", (data) => {
+    socket.to(data.roomId).emit("user_typing", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
